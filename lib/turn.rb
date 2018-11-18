c70b86565536419903538c218b58758e8905790c
Arthur Torres <arthurtorres002@gmail.com>
2018-11-18 00:05:45 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -3,7 +3,7 @@ def turn(board)
   input = gets.strip
   input_to_index(input)
   
-  if valid_move?(board,index)
+  if valid_move?(board, position_taken?(board, index))
     move(board,index,character)
   else
     puts "Invalid Move"
